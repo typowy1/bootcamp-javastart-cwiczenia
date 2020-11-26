569e871e85a995bd3869f8b3fa5d883b24088d6b
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-11-26 15:15:54 +0100

    p

---
+++ src/cwiczenia/lekcja17woddzielnymrepo/lekcja17wcwiczeniachwfolderze/cwiczenie1/OptionalExerciseTest.java
@@ -0,0 +1,60 @@
+package cwiczenia.lekcja17woddzielnymrepo.lekcja17wcwiczeniachwfolderze.cwiczenie1;
+
+
+public class OptionalExerciseTest {
+
+    //  Generalnie rozwiązanie jest w folderze lekcja 17, na tamtym branchu tu tylko przykład
+
+    // to powinno być w test jav dlatego ze to testy, to tylko przykład, bez poma i junita nie zadziała
+
+
+//    private OptionalExercise optionalExercise;
+//
+//    @Before public void init() {
+//        optionalExercise = new OptionalExercise();
+//    }
+//
+//    @Test public void shouldCreateEmptyOptional() {
+//        Optional optional = optionalExercise.createEmptyOptional();
+//
+//        // then
+//        assertThat(optional.isPresent(), is(false));
+//    }
+//
+//    @Test public void shouldOptionalWithValue() {
+//        Optional optional = optionalExercise.createOptionalWithValue();
+//
+//        // then
+//        assertThat(optional.isPresent(), is(true));
+//    }
+//
+//    @Test public void shouldOptionalWithGivenValue() {
+//        Optional<String> optional = optionalExercise.createOptionalWithValue("Test value");
+//
+//        // then
+//        assertThat(optional.isPresent(), is(true));
+//        assertThat(optional.get(), is("Test value"));
+//    }
+//
+//    @Test public void shouldOptionalWithGivenValueAndDefaultValue() {
+//        // given
+//        Optional<String> optional = Optional.of("Test value");
+//
+//        // when
+//        String result = optionalExercise.getOptionalValueOrDefault(optional, "Default value");
+//
+//        // then
+//        assertThat(result, is("Test value"));
+//    }
+//
+//    @Test public void shouldOptionalWithGivenValueAndDefaultValue2() {
+//        // given
+//        Optional<String> optional = Optional.empty();
+//
+//        // when
+//        String result = optionalExercise.getOptionalValueOrDefault(optional, "Default value");
+//
+//        // then
+//        assertThat(result, is("Default value"));
+//    }
+}
