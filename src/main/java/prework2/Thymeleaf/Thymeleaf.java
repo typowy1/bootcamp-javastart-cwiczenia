46c3880f366025ac4e0264c84ee50ef5c0420e0e
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-01-07 19:10:42 +0100

    c

---
+++ src/main/java/prework2/Thymeleaf/Thymeleaf.java
@@ -0,0 +1,8 @@
+package prework2.Thymeleaf;
+
+public class Thymeleaf {
+
+    //   https://javastart.pl/kurs/spring/spring-spring-mvc/lekcja/spring-mvc-%2B-thymeleaf
+    //P:\Projekty\Bootcamp java\Spring ćwiczenia i prweork\lekcja21Mvc i thymeleaf
+
+}
